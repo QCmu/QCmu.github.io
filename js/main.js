15e6d272302b335f022a177dd7f6450be0d21eaf
qiaochuMU <mu.qiaochu@gmail.com>
2016-12-15 18:45:51 -0500

    Coming soon! Estimated Beta Release Date: Jan. 09,2017

--- js/main.js
+++ js/main.js
@@ -1,94 +1,141 @@
-jQuery(document).ready(function($){
-	//on mobile - open/close primary navigation clicking/tapping the menu icon
-	$('.cd-primary-nav').on('click', function(event){
-		if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
-	});
-
-	//upload videos if not on mobile
-	uploadVideo($('.cd-hero-slider'));
-
-	//change visible slide
-	$('.cd-slider-nav li').on('click', function(event){
-		event.preventDefault();
-		var selectedItem = $(this);
-		if(!selectedItem.hasClass('selected')) {
-			// if it's not already selected
-			var selectedPosition = selectedItem.index(),
-				activePosition = $('.cd-hero-slider .selected').index();
-			if( activePosition < selectedPosition) {
-				nextSlide($('.cd-hero-slider'), $('.cd-slider-nav'), selectedPosition);
-			} else {
-				prevSlide($('.cd-hero-slider'), $('.cd-slider-nav'), selectedPosition);
-			}
-
-			updateNavigationMarker(selectedPosition+1);
-		}
-	});
-
-	function nextSlide(container, pagination, n){
-		var visibleSlide = container.find('.selected'),
-			navigationDot = pagination.find('.selected');
-		
-		visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-			visibleSlide.removeClass('is-moving');
-		});
-		
-		container.children('li').eq(n).addClass('selected from-right').prevAll().addClass('move-left');
-		navigationDot.removeClass('selected')
-		pagination.find('li').eq(n).addClass('selected');
-
-		checkVideo(visibleSlide, container, n);
-	}
-
-	function prevSlide(container, pagination, n){
-		var visibleSlide = container.find('.selected'),
-			navigationDot = pagination.find('.selected');
-		
-		visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-			visibleSlide.removeClass('is-moving');
-		});
-
-		container.children('li').eq(n).addClass('selected from-left').removeClass('move-left').nextAll().removeClass('move-left');
-		navigationDot.removeClass('selected');
-		pagination.find('li').eq(n).addClass('selected');
-
-		checkVideo(visibleSlide, container, n);
-	}
-
-	function uploadVideo(container) {
-		container.find('.cd-bg-video-wrapper').each(function(){
-			var videoWrapper = $(this);
-			if( videoWrapper.is(':visible') ) {
-				// if visible - we are not on a mobile device 
-				var	videoUrl = videoWrapper.data('video'),
-					video = $('<video loop><source src="'+videoUrl+'.mp4" type="video/mp4" /><source src="'+videoUrl+'.webm" type="video/webm" /></video>');
-				video.appendTo(videoWrapper);
-				// play video if first slide
-				if(videoWrapper.parents('.cd-bg-video.selected').length > 0) video.get(0).play();
-			}
-		});
-	}
-
-	function checkVideo(hiddenSlide, container, n) {
-		//check if a video outside the viewport is playing - if yes, pause it
-		if( hiddenSlide.find('video').length > 0 ) hiddenSlide.find('video').get(0).pause();
-
-		//check if the select slide contains a video element - if yes, play the video
-		if( container.children('li').eq(n).find('video').length > 0 ) container.children('li').eq(n).find('video').get(0).play();
-	}
-
-	function updateNavigationMarker(n) {
-		$('.cd-marker').removeClassPrefix('item').addClass('item-'+n);
-	}
-
-	$.fn.removeClassPrefix = function(prefix) {
-		//remove all classes starting with 'prefix'
-	    this.each(function(i, el) {
-	        var classes = el.className.split(" ").filter(function(c) {
-	            return c.lastIndexOf(prefix, 0) !== 0;
-	        });
-	        el.className = $.trim(classes.join(" "));
-	    });
-	    return this;
-	};
+ $('#parallax').parallax({
+    invertX: true,
+    invertY: true,
+    scalarX: 10,
+     frictionY: .1
 });
+
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 120,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+
+
+
+var clock;
+		
+		$(document).ready(function() {
+			var clock;
+
+			clock = $('.clock').FlipClock({
+		        clockFace: 'DailyCounter',
+		        autoStart: false,
+		        callbacks: {
+		        	stop: function() {
+		        		$('.message').html('Say hello to Qiaochu\'s Portfokio!' )
+		        	}
+		        }
+		    });
+				    
+		    clock.setTime(15*24*3600);
+		    clock.setCountdown(true);
+		    clock.start();
+
+		});
