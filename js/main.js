6d3a5e60026ccd315e504f6ff43310a73867ad5d
qiaochuMU <hi@beatkcal.com>
2018-09-15 13:45:04 -0700

    side-nav debug; glasses color changed; font-family debug

--- js/main.js
+++ js/main.js
@@ -101,34 +101,34 @@
 
 function smoothScrollToAnchor() {
  $("#projects").click(function() {
-  TweenMax.to(window, 2, { scrollTo: { y: $(window).height() }, ease: Power2.easeOut });
+  TweenMax.to(window, 1.5, { scrollTo: { y: $(window).height() }, ease: Power2.easeOut });
       var temp=$( "a[title='About and Contact']");
   TweenMax.set(temp, {className:"-=active"});
     var temp=$( "a[title='Works']");
   TweenMax.set(temp, {className:"+=active"});
  });
 $("#contact").click(function() {
-  TweenMax.to(window, 2, { scrollTo: { y: $('#about').position().top }, ease: Power2.easeOut });
+  TweenMax.to(window, 1.5, { scrollTo: { y: $('#about').position().top }, ease: Power2.easeOut });
    var temp=$( "a[title='About and Contact']");
   TweenMax.set(temp, {className:"+=active"});
     var temp=$( "a[title='Works']");
   TweenMax.set(temp, {className:"-=active"});
  $("#b-1").click(function() {
-  TweenMax.to(window, 2, { scrollTo: { y: $('#project-culc').position().top }, ease: Power2.easeOut });
+  TweenMax.to(window, 1.5, { scrollTo: { y: $('#project-culc').position().top }, ease: Power2.easeOut });
       var temp=$( "a[title='About and Contact']");
   TweenMax.set(temp, {className:"-=active"});
     var temp=$( "a[title='Works']");
   TweenMax.set(temp, {className:"+=active"});
  });
      $("#b-2").click(function() {
-  TweenMax.to(window, 2, { scrollTo: { y: $('#project-consensus').position().top }, ease: Power2.easeOut });
+  TweenMax.to(window, 1.5, { scrollTo: { y: $('#project-consensus').position().top }, ease: Power2.easeOut });
       var temp=$( "a[title='About and Contact']");
   TweenMax.set(temp, {className:"-=active"});
     var temp=$( "a[title='Works']");
   TweenMax.set(temp, {className:"+=active"});
  });
      $("#b-3").click(function() {
-  TweenMax.to(window, 2, { scrollTo: { y: $('#more').position().top }, ease: Power2.easeOut });
+  TweenMax.to(window, 1.5, { scrollTo: { y: $('#more').position().top }, ease: Power2.easeOut });
       var temp=$( "a[title='About and Contact']");
   TweenMax.set(temp, {className:"-=active"});
     var temp=$( "a[title='Works']");
@@ -161,7 +161,7 @@ function scrollSpy(){
      portfolioScrollMagic();
      projectsHoverStates();
      smoothScrollToAnchor();
-     scrollSpy();
+    // scrollSpy();
  });
 
 
